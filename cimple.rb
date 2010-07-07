7d3608e717898235ba97c9d1d906a301535e02d5
Rob Cameron <cannikinn@gmail.com>
2010-07-06 22:20:30 -0700

    Last runs written out to .yml file

--- cimple.rb
+++ cimple.rb
@@ -3,37 +3,29 @@ require 'bundler'
 #Bundler.setup
 require 'sinatra'
 require 'logger'
-
-class Array
-  def every(count)
-    chunks = []
-    each_with_index do |item, index|
-      chunks << [] if index % count == 0
-      chunks.last << item
-    end
-    chunks
-  end
-  alias / every
-end
+require 'helpers'
 
 configure do
-  set :logger, Logger.new(STDOUT)
+  set :logger, Logger.new(ARGV[0] || STDOUT)
   set :commands, YAML.load(File.open(File.join(File.dirname(File.expand_path(__FILE__)),'commands.yml')))
+  set :last_run, File.join(File.dirname(File.expand_path(__FILE__)),'last_run.yml')
 end
 
-helpers do
-  def render(output)
-    "<pre>#{output.join('<br><br>')}</pre>"
-  end
+# show output of latest run
+get '/' do
+  @output = YAML.load(File.read(options.last_run))
+  @run_at = File.ctime(options.last_run)
+  erb :index
 end
 
+# execute 
 get '/:hook' do
   options.logger.info "#{params[:hook]} hook called"
   output = []
   options.commands[params[:hook]].each do |command|
-    output << command.keys.first
-    output << `#{command.values.first} 2>&1`
+    options.logger.info "  Executing #{command.keys.first}: #{command.values.first}"
+    output << { command.keys.first => `#{command.values.first} 2>&1` }
   end
-  render output
+  File.open(options.last_run, 'w') { |f| f.puts(output.to_yaml) }
+  return ''
 end
-
