eacb2fb282115e8bff14f0e28ac09a2ebc1f1f60
Rob Cameron <cannikinn@gmail.com>
2010-07-06 22:54:23 -0700

    Keeping track of stdout and stderr seperately, added layout and view for display

--- cimple.rb
+++ cimple.rb
@@ -3,28 +3,35 @@ require 'bundler'
 #Bundler.setup
 require 'sinatra'
 require 'logger'
+require 'open3'
 require 'helpers'
 
 configure do
-  set :logger, Logger.new(ARGV[0] || STDOUT)
+  set :logger, Logger.new(ARGV[1] || STDOUT)
   set :commands, YAML.load(File.open(File.join(File.dirname(File.expand_path(__FILE__)),'commands.yml')))
   set :last_run, File.join(File.dirname(File.expand_path(__FILE__)),'last_run.yml')
 end
 
 # show output of latest run
 get '/' do
-  @output = YAML.load(File.read(options.last_run))
-  @run_at = File.ctime(options.last_run)
-  erb :index
+  begin
+    @output = YAML.load(File.read(options.last_run))
+    @run_at = File.ctime(options.last_run)
+    erb :index
+  rescue Errno::ENOENT
+    'Cimple has not been run yet! <a href="http://github.com/cannikin/cimple/blob/master/README">Set up your hooks</a> first, then check back.'
+  end
 end
 
 # execute 
 get '/:hook' do
   options.logger.info "#{params[:hook]} hook called"
-  output = []
+  output = { params[:hook] => [] }
   options.commands[params[:hook]].each do |command|
     options.logger.info "  Executing #{command.keys.first}: #{command.values.first}"
-    output << { command.keys.first => `#{command.values.first} 2>&1` }
+    stdin, stdout, stderr = Open3.popen3(command.values.first)
+    output[params[:hook]] << { command.keys.first => { 'stdout' => stdout.readlines.join, 'stderr' => stderr.readlines.join }}
+    # output << { command.keys.first => `#{command.values.first} 2>&1` }
   end
   File.open(options.last_run, 'w') { |f| f.puts(output.to_yaml) }
   return ''
